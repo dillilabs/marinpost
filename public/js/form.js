7beeecbddf797af334f556b784cb6ce19452d6d9
Steve Pedersen <stvpedersen@gmail.com>
2015-02-04 22:31:03 -0500

    media link form

--- public/js/form.js
+++ public/js/form.js
@@ -5,6 +5,7 @@ $(function() {
     var form = $(this);
     var arraySelects = form.find('select.array');
     var optionalFieldToggles = form.find('.optional-field.toggle');
+    var mediaType = form.find('input[type=radio][name=mediaType]');
 
     // show/hide optional field inputs
     optionalFieldToggles.change(function() {
@@ -13,7 +14,19 @@ $(function() {
       toggle.next('.optional-field.inputs').toggle();
     });
 
-    form.submit(function() {
+    mediaType.change(function() {
+      var type = this.value;
+
+      form.find('input#mediaType').val(type);
+      form.find('input.media, label.media').hide().end().find('input.media.'+type, 'label.media.'+type).show();
+    });
+
+    form.submit(function(e) {
+      var selectedMediaType = form.find('input[type=radio][name=mediaType]:checked');
+
+      if (selectedMediaType.length > 0) {
+        form.find('input.media').not('.'+selectedMediaType.val()).removeAttr('name');
+      }
 
       // Array type input fields (eg fields[foo][]) with empty values
       // are not correctly validated for presence by Craft.
