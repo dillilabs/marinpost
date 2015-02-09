cba65a07e82b6b6c7b031375482b8a52e684f138
Steve Pedersen <stvpedersen@gmail.com>
2015-02-09 15:39:08 -0500

    add another image / document

--- public/js/form.js
+++ public/js/form.js
@@ -1,27 +1,60 @@
 $(function() {
+  // Front-end content submission forms
   var forms = $('form.content-submission');
 
   forms.each(function() {
     var form = $(this);
+
+    // Primary Location / Topic
     var arraySelects = form.find('select.array');
-    var optionalFieldToggles = form.find('.optional-field.toggle');
+
+    // Link to Media
     var mediaType = form.find('input[type=radio][name=mediaType]');
 
+    // TODO deprecated
+    var optionalFieldToggles = form.find('.optional-field.toggle');
+
+    // Secondary Locations / Topics, Images, Documents
+    var multipleFieldLinks = form.find('a.multiple-field');
+    var addMultipleFieldLink = multipleFieldLinks.filter('.add');
+    var removeMultipleFieldLink = multipleFieldLinks.filter('.remove');
+    var multipleFieldInputs = form.find('.multiple-field.inputs');
+
+    // Add Location/Topic/Image/Document
+    addMultipleFieldLink.click(function(e) {
+      e.preventDefault();
+      var link = $(this);
+      var idSuffix = link.attr('id').split('-').splice(1, 2).join('-');
+      $('#input-'+idSuffix).show();
+      link.nextAll('a.multiple-field.add:first').show();
+      link.hide();
+    });
+
+    // Remove Location/Topic/Image/Document
+    removeMultipleFieldLink.click(function(e) {
+      e.preventDefault();
+      var link = $(this);
+      var idSuffix = link.attr('id').split('-').splice(1, 2).join('-');
+      link.closest('.multiple-field.inputs').hide();
+      $('#add-'+idSuffix).show();
+    });
+
+    // TODO deprecated
     // show/hide optional field inputs
     optionalFieldToggles.change(function() {
       var toggle = $(this);
-
       toggle.next('.optional-field.inputs').toggle();
     });
 
+    // Link to Media
     mediaType.change(function() {
       var type = this.value;
-
       form.find('input#mediaType').val(type);
       form.find('input.media, label.media').hide().end().find('input.media.'+type, 'label.media.'+type).show();
     });
 
     form.submit(function(e) {
+      // Link to Media only
       var selectedMediaType = form.find('input[type=radio][name=mediaType]:checked');
 
       if (selectedMediaType.length > 0) {
@@ -40,6 +73,7 @@ $(function() {
         }
       });
 
+      // TODO deprecated
       // Remove optional field inputs if hidden
       optionalFieldToggles.each(function() {
         var toggle = $(this);
@@ -48,6 +82,9 @@ $(function() {
           toggle.next('.optional-field.inputs').remove();
         }
       });
+
+      // Remove hidden Locations, Topics, Images, Documents
+      multipleFieldInputs.not(':visible').remove();
     });
   });
 });
