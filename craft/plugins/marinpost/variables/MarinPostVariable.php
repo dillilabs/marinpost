ceaf8418da24e034ffb1aff422b73074f183cbbf
Steve Pedersen <stvpedersen@gmail.com>
2015-02-14 12:57:15 -0500

    list s3 assets per user id; update s3 asset index programmatically

--- craft/plugins/marinpost/variables/MarinPostVariable.php
+++ craft/plugins/marinpost/variables/MarinPostVariable.php
@@ -34,33 +34,98 @@ class MarinPostVariable
         $this->form = $this->postObject->prepareData()->getFormInputs();
     }
 
+    /* Return AWS Access Key ID for direct upload to S3
+     */
     public function awsAccessKey($optional = null)
     {
         return AWS_ACCESS_KEY_ID;
     }
 
+    /* Return AWS S3 bucket for direct upload to S3
+     */
     public function s3Bucket($optional = null)
     {
         return S3_BUCKET;
     }
 
+    /* Return AWS S3 policy for direct upload to S3
+     */
     public function s3Policy($optional = null)
     {
         return $this->form['policy'];
     }
 
-    // debug
-    public function jsonS3Policy($optional = null) {
-        return $this->postObject->getJsonPolicy();
-    }
-
+    /* Return AWS signature for direct upload to S3
+     */
     public function awsSignature($optional = null)
     {
         return $this->form['signature'];
     }
 
+    // not currently used
     public function uniqid($optional = null)
     {
         return uniqid();
+    }
+
+    // debug only
+    public function jsonS3Policy($optional = null)
+    {
+        return $this->postObject->getJsonPolicy();
+    }
+
+    /* Return virtual sub-directory on S3 for the current user
+     */
+    public function s3FolderNameForCurrentUser()
+    {
+        return craft()->userSession->isLoggedIn() ? craft()->userSession->id : null;
+    }
+
+    /* Return Asset folder for current user's virtual sub-directory on S3
+     */
+    public function s3FolderForCurrentUser($assetSourceId)
+    {
+        $folder = null;
+
+        if (craft()->userSession->isLoggedIn()) {
+            $folder = craft()->assets->findFolder([
+                'sourceId' => $assetSourceId,
+                'name' => $userId = craft()->userSession->id
+            ]);
+        }
+
+        return $folder;
+    }
+
+    /* Update index for S3 Asset source
+     *
+     * Need to do this asynchronously after one or more files are uploaded direct to S3.
+     * Somehow...
+     *
+     * Would be good to only process index for new files. But how?
+     * The offset (of the S3 assets) is via alphabetic sort order, rather than upload date.
+     */
+    public function updateAssetIndex($sourceId)
+    {
+        if (! craft()->userSession->isLoggedIn()) {
+            return null;
+        }
+
+        $sessionId = craft()->assetIndexing->getIndexingSessionId();
+
+        $indexList = craft()->assetIndexing->getIndexListForSource($sessionId, $sourceId);
+
+        $processed = array();
+
+        if (empty($indexList['error']))
+        {
+            for ($i = 0; $i < $indexList['total']; $i++)
+            {
+                $result = craft()->assetIndexing->processIndexForSource($sessionId, $i, $sourceId);
+                array_push($processed, $result);
+            }
+        }
+
+        return array('indexList' => $indexList, 'processed' => $processed);
     }
 }
