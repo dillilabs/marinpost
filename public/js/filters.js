7ddb0725dc673f29d82e308ad6d9c9461ba3ab97
Steve Pedersen <stvpedersen@gmail.com>
2015-02-25 20:04:43 -0500

    integrated authors into filter/more

--- public/js/filters.js
+++ public/js/filters.js
@@ -7,12 +7,22 @@
             var section = document.location.pathname.split('/')[1];
             var loadMoreLink = $('#load-more-content'); // TODO activate on scroll
           
+            var filterType = function(e) {
+              if (e.is('.location')) {
+                return '.location';
+              } else if (e.is('.topic')) {
+                return '.topic';
+              } else if (e.is('.author')) {
+                return '.author';
+              }
+            };
+
             var activeFilters = function(type) {
               return filters.filter('.'+type+':checked').map(function() { return this.value; }).get().join();
             };
           
             var refreshViews = function() {
-              filteredContent.load('/filter/'+section+'?locations='+activeFilters('location')+'&topics='+activeFilters('topic'));
+              filteredContent.load('/filter/'+section+'?locations='+activeFilters('location')+'&topics='+activeFilters('topic')+'&authors='+activeFilters('author'));
             };
           
             var currentContentLength = function() {
@@ -21,7 +31,7 @@
           
             var loadMoreContent = function() {
               $.get(
-                '/more/'+section+'?locations='+activeFilters('location')+'&topics='+activeFilters('topic')+'&offset='+currentContentLength(),
+                '/more/'+section+'?locations='+activeFilters('location')+'&topics='+activeFilters('topic')+'&authors='+activeFilters('author')+'&offset='+currentContentLength(),
                 function(data) {
                   filteredContent.append(data);
                 }
@@ -30,7 +40,7 @@
           
             filters.click(function() {
               var filter = $(this);
-              var type = filter.is('.location') ? '.location' : '.topic';
+              var type = filterType(filter);
               var typeFilters;
           
               if (filter.is(':checked')) {
@@ -45,7 +55,7 @@
           
             noFilters.click(function() {
               var noFilter = $(this);
-              var type = noFilter.is('.location') ? '.location' : '.topic';
+              var type = filterType(noFilter);
           
               if (noFilter.is(':checked')) {
                 filters.filter(type).prop('checked', false);
