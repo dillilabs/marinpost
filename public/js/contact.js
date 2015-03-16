1579ac225ee1840ddfdc9bb362eaf8c2df6de7ae
Steve Pedersen <stvpedersen@gmail.com>
2015-03-16 15:18:08 -0400

    contact form validation

---
+++ public/js/contact.js
@@ -0,0 +1,62 @@
+(function($) {
+    $.fn.contact  = function(options) {
+        return this.each(function() {
+            var form = $(this);
+
+            //-----------------------
+            // Selectors
+            //-----------------------
+
+            var name = form.find('input[name=fromName]');
+            var email = form.find('input[name=fromEmail]');
+            var subject = form.find('input[name=subject]');
+            var message = form.find('textarea[name=message]');
+
+            //-----------------------
+            // Functions
+            //-----------------------
+
+            var validField = function(field, label) {
+                if (field.val().trim().length == 0) {
+                    field.after('<ul class="errors"><li>'+label+' cannot be blank.</li></ul>');
+                    return false;
+                }
+
+                return true;
+            };
+
+            // Email must be at least 5 chars and contain an @
+            var validEmail = function(field) {
+                var email = field.val();
+
+                if (email.length < 5 || email.indexOf('@') < 1) {
+                    field.after('<ul class="errors"><li>Email is not valid.</li></ul>');
+                    return false;
+                }
+
+                return true;
+            };
+
+            //-----------------------
+            // Hooks
+            //-----------------------
+
+            form.submit(function(e) {
+                var validForm = true;
+                
+                form.find('ul.errors').remove();
+
+                if (!validField(email, 'Email')) {
+                    validForm = false;
+                } else if (!validEmail(email)) {
+                    validForm = false;
+                }
+                if (!validField(name, 'Name')) validForm = false;
+                if (!validField(subject, 'Subject')) validForm = false;
+                if (!validField(message, 'Message')) validForm = false;
+
+                return validForm;
+            });
+        });
+    };
+}(jQuery));
