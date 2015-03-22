0140ed5a84cdd4b86f5a679bf811e444ee39802b
Steve Pedersen <stvpedersen@gmail.com>
2015-03-22 13:59:53 -0400

    letter date picker

--- public/js/letters.js
+++ public/js/letters.js
@@ -1,11 +1,13 @@
 $(function() {
-  var selectDate = $('input#select-letter-date');
+  var selectDate = $('div#select-letter-date');
   var baseUrl = '/letters/';
   var toggleComments = $('#toggle-letter-comments');
   var comments = $('#letter-comments');
+  var dateFromUrl = document.location.pathname.match(/\/(\d{4}\/\d{2}\/\d{2})/);
 
   selectDate.datepicker({
     dateFormat: 'yy/mm/dd',
+    defaultDate: dateFromUrl ? dateFromUrl[1] : +0,
     onSelect: function() {
       document.location = baseUrl + this.value;
     }
