7c180650f62285775a73bd0c8c7384aec4d9481c
Steve Pedersen <stvpedersen@gmail.com>
2015-03-23 11:30:10 -0700

    always show letter comments

--- public/js/letters.js
+++ public/js/letters.js
@@ -1,8 +1,6 @@
 $(function() {
   var selectDate = $('div#select-letter-date');
   var baseUrl = '/letters/';
-  var toggleComments = $('#toggle-letter-comments');
-  var comments = $('#letter-comments');
   var dateFromUrl = document.location.pathname.match(/\/(\d{4}\/\d{2}\/\d{2})/);
 
   selectDate.datepicker({
@@ -14,9 +12,4 @@ $(function() {
   });
 
   $('.sub-nav').addClass('archive');
-
-  toggleComments.click(function() {
-    comments.toggle();
-    toggleComments.text((comments.is(':visible') ? 'Hide' : 'Show') + ' Comments');
-  });
 });
