6ed3f8e6618504e24d34fd1e065989e11d163a95
Steve Pedersen <stvpedersen@gmail.com>
2015-03-23 17:05:34 -0700

    about/contributors

---
+++ public/js/about.js
@@ -0,0 +1,17 @@
+$(function() {
+  var contributorRadioButtons = $('ul.contributors');
+
+  if (document.location.pathname.match(/^\/about\/contributors/)) {
+    contributorRadioButtons.show();
+  }
+
+  if (m = document.location.pathname.match(/(^\/about\/contributors\/\d+\/\S+)/)) {
+    console.log(m[1]);
+    contributorRadioButtons.find('input[value$="'+m[1]+'"]').attr('checked', 'checked');
+  }
+
+  contributorRadioButtons.find('input').click(function(e) {
+    e.preventDefault();
+    document.location = this.value;
+  });
+});
