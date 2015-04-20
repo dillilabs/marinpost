a30dec0c23f5651e4ac12b315973ac9c3df3d8f4
willhaus <willy@swayspace.com>
2015-04-20 15:39:28 -0400

    updating nav and subnav in all pages for responsiveness

--- public/js/main.js
+++ public/js/main.js
@@ -30,8 +30,17 @@ $(function() {
     }, 200);
   });
 
-  $('.show-menu').click(function() {
-    $('nav').slideToggle();
+  $('.show-menu , .close').click(function() {
+      // Set the effect type
+    var effect = 'slide';
+
+    // Set the options for the effect type chosen
+    var options = { direction: 'left' };
+
+    // Set the duration (default: 400 milliseconds)
+    var duration = 500;
+    $('nav ul ul').hide(effect, options, duration);
+    $('nav').toggle(effect, options, duration);
   });
 
   $('.my-content a').click(function() {
@@ -40,6 +49,31 @@ $(function() {
 
   $('.sidebar, #support, #mini-menu').show();
 
+  $('.mobile').click(function() {
+    var effect = 'slide';
+
+    // Set the options for the effect type chosen
+    var options = { direction: 'left' };
+
+    // Set the duration (default: 400 milliseconds)
+    var duration = 500;
+    $('nav ul ul').hide(effect, options, duration);
+    $(this).next('ul').toggle(effect, options, duration);
+  });
+  
+  $('.sub-nav').each(function(){
+      if($(this).children().length == 0){
+        $(this).hide();
+      }
+      if($(this).children(':visible').length == 0){
+        $(this).hide();
+      }
+    });
+    
+    $('.filter h4, .sub-nav > h4').click(function() {
+       $(this).siblings().slideToggle();
+       $('.sub-nav h2').hide();
+    });
 });
 
 $(window).on('resize', function(){
