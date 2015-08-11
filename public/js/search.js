349259f1f6ec6b32923587a1a3f8ba5ebfec61ed
Steve Pedersen <stvpedersen@gmail.com>
2015-08-11 09:59:36 -0700

    add 'back to search' to blog/notice detail pages when navigated from /search

--- public/js/search.js
+++ public/js/search.js
@@ -1,6 +1,7 @@
 $(function() {
   var simple = $('form.simple');
   var advanced = $('form.advanced');
+  var results = $('ul.posts.search');
 
   simple.submit(function() {
     $(this).addClass('processing');
@@ -8,5 +9,10 @@ $(function() {
 
   advanced.submit(function() {
     $(this).addClass('processing').find('input[type=submit]').val('Processing').attr('disabled', 'disabled');
+  });
+
+  results.on('click', 'a.detail', function(e) {
+    e.preventDefault();
+    document.location = this.href + document.location.search;
   });
 });
