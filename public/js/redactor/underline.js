08842afbe4d594244cd6e33bee121226927b5c93
Steve Pedersen <stvpedersen@gmail.com>
2016-06-20 11:25:11 -0700

    Copy Redactor I resources to the front-end

---
+++ public/js/redactor/underline.js
@@ -0,0 +1,20 @@
+/**
+ * @see http://imperavi.com/redactor/examples/buttons-underline/
+ */
+
+if (!RedactorPlugins) var RedactorPlugins = {};
+ 
+RedactorPlugins.underline = function()
+{
+    return {
+        init: function()
+        {
+            var button = this.button.addAfter('italic', 'underline', 'Underline');
+            this.button.addCallback(button, this.underline.format);
+        },
+        format: function()
+        {
+            this.inline.format('u');
+        }
+    };
+};
