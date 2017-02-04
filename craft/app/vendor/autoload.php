6442350e5d158d99a36454755d9dfc9cea17f035
Steve Pedersen <speder@pacbell.net>
2017-02-03 19:54:17 -0800

    Upgrade to Craft 2.6.2960

--- craft/app/vendor/autoload.php
+++ craft/app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit3714fe01d0d2977add0f422140552c5e::getLoader();
+return ComposerAutoloaderInitacb46ae68d0b71ce5ad0edd663c0a661::getLoader();
